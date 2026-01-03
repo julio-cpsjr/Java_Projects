b401574db5d19465087ee24e00352bf148375b29
Julio Cesar <juliocpsjunior@gmail.com>
2026-01-03 16:08:13 -0300

    Projeto processo seletivo

--- dio-projects/processo-seletivo/src/processoSeletivo.java
+++ dio-projects/processo-seletivo/src/processoSeletivo.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class processoSeletivo {
-    public static void main() {
-        System.out.println("Processo Seletivo");
-    }
+      public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Processo Seletivo");
+          System.out.println("Qual a sua pretenção salarial?");
+          double salario = sc.nextDouble();
+          analisarCandidato(salario);
+      }
+      static void analisarCandidato(double salarioPretendido) {
+          if (salarioPretendido < 2000.0) {
+              System.out.println("LIGAR PARA CANDIDATO");
+          } else if (salarioPretendido == 2000.0) {
+              System.out.println("LIGAR PARA CANDIDATO COM CONTRA PROPOSTA");
+          } else System.out.println("AGUARDANDO RESULTADO DEMAIS CANDIDATOS");
+      }
 }
