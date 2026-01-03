efbdbb9b4d80f9f529cfb337cf9a714d9c2a5070
Julio Cesar <juliocpsjunior@gmail.com>
2026-01-03 16:23:06 -0300

    Criação de métodos para execução do programa

--- dio-projects/processo-seletivo/src/processoSeletivo.java
+++ dio-projects/processo-seletivo/src/processoSeletivo.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class processoSeletivo {
       public static void main(String[] args) {
+          double salarioPretendido = salarioCandidato();
+          analisarCandidato(salarioPretendido);
+      }
+      static double salarioCandidato(){
           Scanner sc = new Scanner(System.in);
           System.out.println("Processo Seletivo");
           System.out.println("Qual a sua pretenção salarial?");
-          double salario = sc.nextDouble();
-          analisarCandidato(salario);
+          return sc.nextDouble();
       }
+
       static void analisarCandidato(double salarioPretendido) {
           if (salarioPretendido < 2000.0) {
               System.out.println("LIGAR PARA CANDIDATO");
