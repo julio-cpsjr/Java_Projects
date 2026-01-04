98c567f9280461e4b51acde520913148660971fe
Julio Cesar <juliocpsjunior@gmail.com>
2026-01-04 17:15:16 -0300

    Correction directory class

--- dio-projects/processo-seletivo/src/Main.java
+++ dio-projects/processo-seletivo/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import br.com.dio.dominio.ProcessoSeletivo;
 
 public class Main {
       public static void main(String[] args) {
--- dio-projects/processo-seletivo/src/br/com/dio/dominio/ProcessoSeletivo.java
+++ dio-projects/processo-seletivo/src/br/com/dio/dominio/ProcessoSeletivo.java
@@ -1,13 +1,15 @@
+package br.com.dio.dominio;
+
 import java.util.Scanner;
 public class ProcessoSeletivo {
-    static double salarioCandidato(){
+    public static double salarioCandidato(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Processo Seletivo");
         System.out.println("Qual a sua pretenção salarial?");
         return sc.nextDouble();
     }
 
-    static void analisarCandidato(double salarioPretendido) {
+    public static void analisarCandidato(double salarioPretendido) {
         if (salarioPretendido < 2000.0) {
             System.out.println("LIGAR PARA CANDIDATO");
         } else if (salarioPretendido == 2000.0) {
