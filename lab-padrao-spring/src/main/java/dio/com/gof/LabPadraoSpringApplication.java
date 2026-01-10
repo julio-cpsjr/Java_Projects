93344ab47205f18d2b3b83db981e6957bff62947
julio-cpsjr <juliocpsjunior@gmail.com>
2026-01-09 23:56:47 -0300

    First Spring Project

---
+++ lab-padrao-spring/src/main/java/dio/com/gof/LabPadraoSpringApplication.java
@@ -0,0 +1,13 @@
+package dio.com.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadraoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadraoSpringApplication.class, args);
+	}
+
+}
